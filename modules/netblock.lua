9eafaa71b0150e1d187a2d3971740780c2c98ef3
Nady <mohamed.amr.nes@gmail.com>
2026-04-08 15:09:58 +0200

    undo unnecessary changes * old testing

--- modules/netblock.lua
+++ modules/netblock.lua
@@ -13,18 +13,14 @@ local block_check = true
 local m = {}
 m.version = "0.3"
 
-function m.make_key(ctx, filename)
-    if string.match(filename, "common\\script\\flow\\Exhibition\\ExhibiUgcCheck.json") then
-        return content_root .. filename
-    elseif block_check and string.match(filename, "common\\script\\flow\\Intro\\DataPackApply\\ProcEnd.json") then --log("Invalid filename: " .. tostring(filename))
-        block_check = false
-        return content_root .. filename
-    end
-end
-
 function m.get_filepath(ctx, filename, key)
     if key then
-        return key
+        if string.match(filename, "common\\script\\flow\\Exhibition\\ExhibiUgcCheck.json") then
+            return content_root .. filename
+        elseif block_check and string.match(filename, "common\\script\\flow\\Intro\\DataPackApply\\ProcEnd.json") then --log("Invalid filename: " .. tostring(filename))
+            block_check = false
+            return content_root .. filename
+        end
     end
 end
 
@@ -35,7 +31,6 @@ function m.init(ctx)
     end
 
     ctx.register("livecpk_get_filepath", m.get_filepath)
-    ctx.register("livecpk_make_key", m.make_key)
 
     for _, s in ipairs(strings) do
         addr, info = memory.search_process(s .. "\x00")
@@ -43,7 +38,7 @@ function m.init(ctx)
             log(string.format('warning: unable to find string: "%s" in memory', s))
         else
             log(string.format('string "%s" found at %s. Replacing with "%s"', s, memory.hex(addr), replacer))
-            memory.write(addr, replacer .. string.rep("\x00", #s - #replacer > 0 and #s - #replacer or 0))
+            memory.write(addr, replacer .. "\x00")
         end
     end
 end
